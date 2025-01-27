a53563c92d47e82d93e9d77618c6cc404573e00a
Mikhail Speaks <mikhailspeaks@gmail.com>
2025-01-27 14:08:19 +0000

    Revert Firebase config to use environment variables

--- src/lib/firebase/firebase.ts
+++ src/lib/firebase/firebase.ts
@@ -3,14 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDIsp4r4rOnrIbHVZahJSCkWpn2z9LnI3U",
-  authDomain: "al-muraja-ah.firebaseapp.com",
-  projectId: "al-muraja-ah",
-  storageBucket: "al-muraja-ah.firebasestorage.app",
-  messagingSenderId: "63031955242",
-  appId: "1:63031955242:web:a015538eaac728eecd1853"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
